274c2da9180057cb582ffa0fdfa05adc013d1069
Marcin Kopański <marcin.kopanski@gmail.com>
2017-10-16 22:19:40 +0200

    Filled in all the templates with HTML

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -9,7 +9,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a test description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Thai_green_chicken_curry_and_roti.jpg/1200px-Thai_green_chicken_curry_and_roti.jpg')
+    new Recipe('A test recipe1', 'This is a test description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Thai_green_chicken_curry_and_roti.jpg/1200px-Thai_green_chicken_curry_and_roti.jpg'),
+    new Recipe('A test recipe2', 'This is a test description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Thai_green_chicken_curry_and_roti.jpg/1200px-Thai_green_chicken_curry_and_roti.jpg'),
+    new Recipe('A test recipe3', 'This is a test description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Thai_green_chicken_curry_and_roti.jpg/1200px-Thai_green_chicken_curry_and_roti.jpg')
   ];
 
   constructor() { }
